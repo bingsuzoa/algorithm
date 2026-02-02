3af0eb6dd8b57268214e64124beb7558ccc26816
mikyeong Kwon <zxc_777@naver.com>
2026-02-03 08:03:11 +0900

    202602023

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,57 @@
 import java.util.*;
 import java.io.*;
 
+
 class Main {
-    static int H,W,N,M;
+    static Set<Integer> set;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        H = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        set = new HashSet<>();
+        sb = new StringBuilder();
 
-        long x = (H / (N + 1));
-        long y = (W / (M + 1));
-        if((H % (N + 1)) != 0) {
-            x ++;
+        while(N --> 0) {
+            String command = br.readLine();
+            if(command.equals("all")) {
+                for(int i = 1; i <= 20; i++) {
+                    set.add(i);
+                }
+                continue;
+            }
+            if(command.equals("empty")) {
+                set.clear();
+                continue;
+            }
+
+            String[] split = command.split(" ");
+            int num = Integer.parseInt(split[1]);
+
+            if(command.contains("add")) {
+                set.add(num);
+            }
+            else if(command.contains("remove")) {
+                if(set.contains(num)) {
+                    set.remove(num);
+                }
+            }
+            else if(command.contains("toggle")) {
+                if(set.contains(num)) {
+                    set.remove(num);
+                } else {
+                    set.add(num);
+                }
+            }
+            else if(command.contains("check")) {
+                if(set.contains(num)) {
+                    sb.append("1").append("\n");
+                } else {
+                    sb.append("0").append("\n");
+                }
+            }
         }
-
-        if(W % (M + 1) != 0) {
-            y++;
-        }
-
-        System.out.println(x * y);
-
+        System.out.println(sb);
     }
 }
