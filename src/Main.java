1624ae6390505cfa1bdf151d2de4353e5bb7a220
mikyeong Kwon <zxc_777@naver.com>
2026-02-05 07:15:18 +0900

    20260205

--- src/Main.java
+++ src/Main.java
@@ -2,75 +2,74 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static Info[] infos;
-    static int M;
+    static char[] chars = new char[]{'a', 'e', 'i', 'o', 'u'};
+    static Set<Character> set = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        for(char c : chars) {
+            set.add(c);
+        }
 
-        infos = new Info[N];
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st1 = new StringTokenizer(br.readLine());
-            int country = Integer.parseInt(st1.nextToken());
-            Info info = new Info(country);
-            infos[i] = info;
+        while(true) {
+            String input = br.readLine();
 
-            for (int j = 0; j < 3; j++) {
-                int num = Integer.parseInt(st1.nextToken());
-                info.setCounts(num, j);
+            if(input.equals("end")) {
+                break;
             }
-        }
-        Arrays.sort(infos);
-
-        if(infos[0].country == M) {
-            System.out.println(1);
-            return;
-        }
-
-        int prize = 1;
-        for(int i = 1 ; i < infos.length;  i++) {
-            if(infos[i].counts[0] == infos[i-1].counts[0] &&
-                infos[i].counts[1] == infos[i - 1].counts[1] &&
-                infos[i].counts[2] == infos[i - 1].counts[2]
-            ) {
-
+            if(isAvail(input)) {
+                System.out.println("<" + input + "> is acceptable.");
             } else {
-                prize = i + 1;
+                System.out.println("<" + input + "> is not acceptable.");
             }
-            if(infos[i].country == M) {
-                System.out.println(prize);
-                return;
-            }
+
         }
     }
-}
+    private static boolean isAvail(String input) {
 
-
-class Info implements Comparable<Info> {
-    int country;
-    int[] counts;
-
-    public Info(int country) {
-        this.country = country;
-        counts = new int[3];
-    }
-
-    public void setCounts(int num, int idx) {
-        counts[idx] = num;
-    }
-
-    @Override
-    public int compareTo(Info o) {
-        if(o.counts[0] == this.counts[0]) {
-            if(o.counts[1] == this.counts[1]) {
-                return o.counts[2] - this.counts[2];
+        char[] inputToChar = input.toCharArray();
+        //모음 하나 반드시 포함
+        boolean isContain = false;
+        for(char c : chars) {
+            for(int i = 0; i  < inputToChar.length; i++) {
+                if(inputToChar[i] == c) {
+                    isContain = true;
+                    break;
+                }
             }
-            return o.counts[1] - this.counts[1];
         }
-        return o.counts[0] - this.counts[0];
+        if(!isContain) {
+            return false;
+        }
+        //모음이 연속 3개, 자음이 연속 3개 불가
+        int size = inputToChar.length;
+        for(int i = 0; i < inputToChar.length; i++) {
+            if(i + 1 < size && i + 2 < size) {
+                char first = inputToChar[i];
+                char second = inputToChar[i+1];
+                char third = inputToChar[i+2];
+
+                if(set.contains(first) && set.contains(second) && set.contains(third)) {
+                    return false;
+                }
+                if(!set.contains(first) && !set.contains(second) && !set.contains(third)) {
+                    return false;
+                }
+            }
+        }
+
+        //같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용한다.
+        for(int i = 0; i < inputToChar.length - 1; i++) {
+            char first = inputToChar[i];
+            char second = inputToChar[i+1];
+            if(first == second) {
+                if(first != 'e' && first != 'o') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
