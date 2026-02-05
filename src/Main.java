4d5549f304787bba1deb9d36b823013660a6614f
mikyeong Kwon <zxc_777@naver.com>
2026-02-06 07:33:32 +0900

    20260206

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,36 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int MOD = 1000000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] dp = new int[N + 1];
-        Arrays.fill(dp, MOD);
+        int N = Integer.parseInt(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
 
-        dp[0] = MOD;
-        dp[1] = MOD;
-        if(N <= 1) {
-            System.out.println(-1);
-            return;
-        }
-        dp[2] = 1;
-        if(N == 2) {
-            System.out.println(1);
-            return;
-        }
-        if(N == 3) {
-            System.out.println(-1);
-            return;
-        }
-        if(N == 4) {
-            System.out.println(2);
-            return;
-        }
-        dp[5] = 1;
+        int[] coins = new int[N];
 
-        for(int i = 3; i < dp.length; i++) {
-            dp[i] = Math.min(dp[i], dp[i-2] + 1);
-            if(i - 5 >= 0) {
-                dp[i] = Math.min(dp[i], dp[i-5] + 1);
+        for(int i = 0; i < coins.length; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        long answer = 0;
+
+        while(K > 0) {
+            long cur = 0;
+            for(long coin : coins) {
+                if(K < coin) {
+                    break;
+                }
+                cur = coin;
             }
+            int count = (int)(K / cur);
+            K -= (count * cur);
+            answer += count;
         }
 
-        if(dp[N] == MOD) {
-            System.out.println(-1);
-        } else {
-            System.out.println(dp[N]);
-        }
+        System.out.println(answer);
     }
 }
