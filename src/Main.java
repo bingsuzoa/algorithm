d1a12225e2f2c7c6b2ccec5d5909dc8c67925ff8
mikyeong Kwon <zxc_777@naver.com>
2026-02-06 08:05:42 +0900

    20260206

--- src/Main.java
+++ src/Main.java
@@ -6,32 +6,46 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+
+        int[] requests = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        long K = Long.parseLong(st.nextToken());
-
-        int[] coins = new int[N];
-
-        for(int i = 0; i < coins.length; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-        }
-
-        long answer = 0;
-
-        while(K > 0) {
-            long cur = 0;
-            for(long coin : coins) {
-                if(K < coin) {
-                    break;
-                }
-                cur = coin;
+        int end = 0;
+        for(int i = 0; i < requests.length; i++) {
+            requests[i] = Integer.parseInt(st.nextToken());
+            if(end < requests[i]) {
+                end = requests[i];
             }
-            int count = (int)(K / cur);
-            K -= (count * cur);
-            answer += count;
         }
 
-        System.out.println(answer);
+        int total = Integer.parseInt(br.readLine());
+
+        int start = 0;
+
+        long max = -1;
+        long temp = 0;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            long sum = 0;
+            for(int request : requests) {
+                if(request > mid) {
+                    sum += mid;
+                } else {
+                    sum += request;
+                }
+            }
+            if(sum <= total) {
+                if(max < sum) {
+                    max = sum;
+                    temp = mid;
+                }
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        System.out.println(temp);
     }
 }
