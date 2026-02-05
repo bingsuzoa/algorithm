a644ad35b522178e88dafd5fbe3ccfb6cb5e6464
mikyeong Kwon <zxc_777@naver.com>
2026-02-05 19:39:49 +0900

    20260205

--- src/Main.java
+++ src/Main.java
@@ -2,87 +2,36 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static char[][] graph;
-    static int[] answer = new int[5];
-    static StringBuilder sb = new StringBuilder();
+    static long[] roads;
+    static long[] liters;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        graph = new char[N][N];
 
-        for(int i = 0; i < graph.length; i++) {
-            String input = br.readLine();
-            for(int j = 0; j < graph[i].length; j++) {
-                graph[i][j] = input.charAt(j);
-            }
+        roads = new long[N - 1];
+        liters = new long[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < roads.length; i++) {
+            roads[i] = Long.parseLong(st.nextToken());
         }
-
-
-        boolean endArm = false;
-        int heartX = -1;
-        int heartY = -1;
-        for(int i = 0; i < graph.length; i++) {
-            for(int j = 0; j < graph[i].length; j++) {
-                if(graph[i][j] == '*') {
-                    heartX = i + 1;
-                    heartY = j;
-                    sb.append((heartX + 1) + " " + (heartY + 1)).append("\n");
-                    countLine(heartX + 1, heartY);
-                    countArms(heartX, heartY);
-                    countLegs(heartX, heartY);
-                    endArm = true;
-                    break;
-                }
-            }
-            if(endArm) {
-                break;
-            }
+        for(int i =0 ; i < liters.length; i++) {
+            liters[i] = Long.parseLong(st1.nextToken());
         }
 
-        for(int value : answer) {
-            sb.append(value + " ");
-        }
-        System.out.println(sb);
-    }
-    private static void countLegs(int x, int heartPos) {
-        int left = 0;
-        int right = 0;
-        for(int i = x+1; i < graph.length; i++) {
-            if(graph[i][heartPos -1] == '*') {
-                left++;
+        long answer = roads[0] * liters[0];
+        long min = liters[0];
+
+        for(int i = 1; i < liters.length - 1; i++) {
+            long curCost = liters[i];
+            if(min > curCost) {
+                min = curCost;
             }
-            if(graph[i][heartPos + 1] == '*') {
-                right++;
-            }
+            answer += (min * roads[i]);
         }
-        answer[3] = left;
-        answer[4] = right;
-    }
-    private static void countArms(int x, int y) {
-        int left = 0;
-        int right = 0;
-        for(int j = y - 1; j >= 0; j--) {
-            if(graph[x][j] == '*') {
-                left++;
-            }
-        }
-        for(int j = y + 1; j < graph[0].length; j++) {
-            if(graph[x][j] == '*') {
-                right ++;
-            }
-        }
-        answer[0] = left;
-        answer[1] = right;
-    }
-    private static void countLine(int x, int y) {
-        int count = 0;
-        for(int i = x; i < graph.length; i++) {
-            if(graph[i][y] == '*') {
-                count++;
-            }
-        }
-        answer[2] = count;
+        System.out.println(answer);
     }
 }
