c60f986e5937b9cd12948588dbbdf28be80aca92
mikyeong Kwon <zxc_777@naver.com>
2026-02-10 08:16:24 +0900

    20260210

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,134 @@
 import java.util.*;
 import java.io.*;
 
+
 class Main {
-    static int[] graph;
-    static Info[] infos;
+    static Map<Integer, Info> map;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        graph = new int[N+1];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i < graph.length; i++) {
-            graph[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int M = Integer.parseInt(br.readLine());
-        infos = new Info[M];
-        for(int i = 0; i < infos.length; i++) {
-            StringTokenizer st1 = new StringTokenizer(br.readLine());
-            int sex = Integer.parseInt(st1.nextToken());
-            int num = Integer.parseInt(st1.nextToken());
-            infos[i] = new Info(sex, num);
-        }
-
-        for(Info info : infos) {
-            if(info.sex == 1) {
-                checkMan(info.num);
-            } else {
-                checkWoman(info.num);
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        int size = 0;
-        for(int i = 1; i < graph.length; i++) {
-            if(size == 20) {
-                sb.append("\n");
-                size = 0;
-            }
-            size ++;
-            sb.append(graph[i] + " ");
-        }
-        System.out.println(sb.toString().trim());
-
-    }
-    private static void checkWoman(int num) {
-        int dist = 0;
-        int start = 0;
-        int end = 0;
-        while(true) {
-            int leftIdx = num - dist;
-            int rightIdx = num + dist;
-
-            if(leftIdx >= 1 && rightIdx < graph.length) {
-                if(graph[leftIdx] == graph[rightIdx]) {
-                    start = leftIdx;
-                    end = rightIdx;
-                    dist++;
-                } else {
-                    break;
+        map = new HashMap<>();
+        sb = new StringBuilder();
+        while(N --> 0) {
+            map.clear();
+            int M = Integer.parseInt(br.readLine());
+            int[] graph = new int[M];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < graph.length; i++) {
+                graph[i] = Integer.parseInt(st.nextToken());
+                if(!map.containsKey(graph[i])) {
+                    map.put(graph[i], new Info());
                 }
-            } else {
-                break;
+                map.get(graph[i]).addCount();
+            }
+
+            for(int key : map.keySet()) {
+                Info info = map.get(key);
+                if(info.count < 6) {
+                    info.isContain = false;
+                }
+            }
+            int rank = 1;
+            for(int i = 0; i < graph.length; i++) {
+                Info info = map.get(graph[i]);
+                if(!info.isContain) continue;
+                info.addScore(rank);
+                rank++;
+            }
+
+            List<Integer> list = new ArrayList<>();
+            int prize = Integer.MAX_VALUE;
+            for(int key : map.keySet()) {
+                Info info = map.get(key);
+                if(!info.isContain) continue;
+                if(prize > info.getSum()) {
+                    prize = info.getSum();
+                }
+            }
+            for(int key : map.keySet()) {
+                Info info = map.get(key);
+                if(!info.isContain) continue;
+                if(info.getSum() == prize) {
+                    list.add(key);
+                }
+            }
+
+            if(list.size() == 1) {
+                sb.append(list.get(0)).append("\n");
+            }
+
+            else if(list.size() > 1) {
+                for(int i = 5; i <= 6; i++) {
+                    if(getPrize(i, list)) {
+                        break;
+                    }
+                }
             }
         }
 
-        for(int i = start; i <= end; i++) {
-            graph[i] = graph[i] == 1 ? 0 : 1;
-        }
+        System.out.println(sb);
     }
-    private static void checkMan(int num) {
-        for(int i = num; i < graph.length; i+=num) {
-            graph[i] = graph[i] == 1 ? 0 : 1;
+
+    private static boolean getPrize(int rank, List<Integer> yebi) {
+        int prize = Integer.MAX_VALUE;
+        List<Integer> list = new ArrayList<>();
+
+        for(int key : yebi) {
+            Info info = map.get(key);
+            if(!info.isContain) continue;
+            if(prize > info.getRemain(rank)) {
+                prize = info.getRemain(rank);
+            }
         }
+        for(int key : yebi) {
+            Info info = map.get(key);
+            if(!info.isContain) continue;
+            if(prize == info.getRemain(rank)) {
+                list.add(key);
+            }
+        }
+        if(list.size() == 1) {
+            sb.append(list.get(0)).append("\n");
+            return true;
+        }
+        return false;
     }
+
 }
 
 class Info {
-    int sex;
-    int num;
+    int count;
+    boolean isContain;
+    int[] score;
+    int idx;
 
-    public Info(int sex, int num) {
-        this.sex = sex;
-        this.num = num;
+    public Info() {
+        this.count = 0;
+        this.isContain = true;
+        this.score = new int[6];
+        this.idx = 0;
+    }
+
+    public void addCount() {
+        count ++;
+    }
+
+    public void addScore(int num) {
+        score[idx] = num;
+        idx++;
+    }
+
+    public int getSum() {
+        int sum = 0;
+        for(int i = 0; i < 4; i++) {
+            sum += score[i];
+        }
+        return sum;
+    }
+    public int getRemain(int rank) {
+        return score[rank -1];
     }
 }
