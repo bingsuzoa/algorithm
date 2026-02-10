1e046b0ec200032d9882bf599e8ffef9a5b33157
mikyeong Kwon <zxc_777@naver.com>
2026-02-11 08:54:55 +0900

    20260211

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,61 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        graph = new int[M];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0 ; i < graph.length; i++) {
-            graph[i] = Integer.parseInt(st.nextToken());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Word[] words = new Word[N];
+        Map<String, Integer> map = new HashMap<>();
+        for(int i =0 ; i < N; i++) {
+            words[i] = new Word(br.readLine());
+            map.put(words[i].word, map.getOrDefault(words[i].word, 0) + 1);
         }
 
-        int min = Math.max(graph[0], N - graph[graph.length -1]);
+        for(Word word : words) {
+            word.count = map.get(word.word);
+        }
+        Arrays.sort(words);
 
-        for(int i = 1; i < graph.length; i++) {
-            int road = graph[i] - graph[i-1];
-
-            if(min * 2 < road) {
-                int diff = road - (min * 2);
-                min += (int)Math.ceil((double)diff / 2);
+        StringBuilder sb = new StringBuilder();
+        Set<String> set = new HashSet<>();
+        for(Word word : words) {
+            if(word.length < K) {
+                continue;
+            }
+            if(!set.contains(word.word)) {
+                set.add(word.word);
+                sb.append(word.word).append("\n");
             }
         }
-        System.out.println(min);
+        System.out.println(sb);
+    }
+}
 
+class Word implements Comparable<Word> {
+    String word;
+    int count;
+    int length;
+
+    public Word(String word) {
+        this.word = word;
+        this.length = word.length();
+        this.count = 0;
+    }
+
+    @Override
+    public int compareTo(Word o) {
+        if(o.count == this.count) {
+            if(o.length == this.length) {
+                return this.word.compareTo(o.word);
+            }
+            return o.length - this.length;
+        }
+        return o.count - this.count;
     }
 }
