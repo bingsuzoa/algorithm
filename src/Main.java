76b87969f76f4acf45d8715a42c5d045ba88f1fd
mikyeong Kwon <zxc_777@naver.com>
2026-02-11 08:18:42 +0900

    20260211

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,39 @@ import java.io.*;
 class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        long cur = 1;
-        int start = 0;
-
-        while(start < input.length()) {
-            String pair = String.valueOf(cur);
-
-            int idx = 0;
-            while(start < input.length()) {
-                boolean isContain = false;
-                for(int i = idx; i < pair.length(); i++) {
-                    if(input.charAt(start) == pair.charAt(i)) {
-                        isContain = true;
-                        start++;
-                        idx = i + 1;
-                        break;
-                    }
-                }
-                if(!isContain) break;
-            }
-            cur++;
+        int[] board = new int[N];
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        for(int i = 0; i < board.length; i++) {
+            board[i] = Integer.parseInt(st1.nextToken());
+            map.putIfAbsent(board[i], 0);
         }
-        System.out.println(cur - 1);
+
+        int left = 0;
+        int right = 0;
+
+        int max = 0;
+        while(right < board.length) {
+            int num = board[right];
+            int count = map.get(num);
+            map.put(num, count + 1);
+
+            while(map.get(num) > K) {
+                int value = board[left];
+                map.put(value, map.get(value) -1);
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+            right++;
+        }
+
+        System.out.println(max);
     }
 }
