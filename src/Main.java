3f21337b9bac84a325da6444d1f616d377fb101f
mikyeong Kwon <zxc_777@naver.com>
2026-02-13 07:21:36 +0900

    20260212

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,49 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        String input = br.readLine();
+
         int N = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int coupon = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int[] graph = new int[N * 2];
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i< N; i++) {
-            graph[i] = Integer.parseInt(br.readLine());
-            graph[i + N] = graph[i];
-            map.put(graph[i], 0);
-        }
 
-        int start = 0;
-        int end = 0;
-        Set<Integer> set = new HashSet<>();
-        int max = 0;
-        int count = 0;
-        while(start < N) {
-            count = end - start + 1;
-            if(count <= k) {
-                map.put(graph[end], map.get(graph[end]) + 1);
-                set.add(graph[end]);
-                if(count == k) {
-                    if(!set.contains(coupon)) {
-                        max = Math.max(max, set.size() + 1);
-                    } else {
-                        max = Math.max(max, set.size());
-                    }
-                }
-                end++;
-            } else {
-                map.put(graph[start], map.get(graph[start]) -1);
-                if(map.get(graph[start]) == 0) {
-                    set.remove(graph[start]);
-                }
-                start++;
+        Queue<Integer> persons = new LinkedList<>();
+        Queue<Integer> hams = new LinkedList<>();
+        for(int i = 0; i < N; i++) {
+            char c = input.charAt(i);
+            if(c == 'P') {
+                persons.add(i);
+            }
+            if(c == 'H') {
+                hams.add(i);
             }
         }
-        System.out.println(max);
+
+        int count = 0;
+
+        while(!persons.isEmpty() && !hams.isEmpty()) {
+            int person = persons.peek();
+            int ham = hams.peek();
+
+            if(ham < person) {
+                if(person - ham <= K) {
+                    count++;
+                    persons.poll();
+                }
+                hams.poll();
+            } else {
+                if(ham - person <= K) {
+                    count++;
+                    hams.poll();
+                }
+                persons.poll();
+            }
+        }
+        System.out.println(count);
+
     }
 }
