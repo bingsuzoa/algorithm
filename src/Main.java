965946067344a126ed702426d6e2955964052443
mikyeong Kwon <zxc_777@naver.com>
2026-02-18 10:14:59 +0900

    20260218

--- src/Main.java
+++ src/Main.java
@@ -2,118 +2,54 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static Map<Character, Integer> index;
-    static char[] goalArray;
-
+    static int[][] graph;
+    static int[] dx = {1, 1, 1};
+    static int[] dy = {0, -1, 1};
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        graph = new int[N][M];
 
-
-        String goal = br.readLine();
-
-        goalArray = new char[goal.length()];
-        for(int i = 0; i < goalArray.length; i++) {
-            goalArray[i] = goal.charAt(i);
-        }
-        Arrays.sort(goalArray);
-
-        int answer = 0;
-        for(int i = 1; i < N; i++) {
-            String input = br.readLine();
-            char[] pair = new char[input.length()];
-
-            for(int j = 0; j < pair.length; j++) {
-                pair[j] = input.charAt(j);
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) {
+                graph[i][j] = Integer.parseInt(st1.nextToken());
             }
-            Arrays.sort(pair);
+        }
 
-            if(goalArray.length == pair.length) {
-                if(isSame(goalArray, pair)) {
-                    answer++;
-                }
-                else if(isChange(goalArray, pair)) {
-                    answer++;
-                }
+        Queue<int[]> queue = new LinkedList<>();
+        for(int j = 0; j < M; j++) {
+            queue.add(new int[]{0, j, -1, graph[0][j]});
+        }
+
+        int answer = Integer.MAX_VALUE;
+
+        while(!queue.isEmpty()) {
+            int[] befo = queue.poll();
+            int x = befo[0];
+            int y = befo[1];
+            int dir = befo[2];
+            int cost = befo[3];
+
+            if(x == N-1) {
+                answer = Math.min(answer, cost);
                 continue;
             }
-            if(check(goalArray, pair)) {
-                answer++;
+
+            for(int i = 0; i < 3; i++) {
+                if(dir == i) continue;
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= N || nx < 0 || ny >= M || ny < 0) continue;
+                queue.add(new int[]{nx, ny, i, cost + graph[nx][ny]});
             }
+
         }
         System.out.println(answer);
-    }
-    private static boolean check(char[] goalArray, char[] pair) {
-        if(Math.abs(goalArray.length - pair.length) != 1) {
-            return false;
-        }
-        if(goalArray.length < pair.length) {
-            return rotate(goalArray, pair);
-        }
-        return rotate(pair, goalArray);
-    }
-    private static boolean rotate(char[] min, char[] many) {
-        boolean[] checked = new boolean[many.length];
 
-        for(int i =0 ; i < min.length; i++) {
-            char c = min[i];
-            boolean isExist = false;
-            for(int j = 0 ; j < many.length; j++) {
-                if(!checked[j] && c == many[j]) {
-                    isExist = true;
-                    checked[j] = true;
-                    break;
-                }
-            }
-            if(!isExist) {
-                return false;
-            }
-        }
-        int count = 0;
-        for(int i = 0; i < checked.length; i++) {
-            if(!checked[i]) {
-                count++;
-            }
-        }
-        if(count == 1) return true;
-        return false;
-    }
-    private static boolean isChange(char[] goalArray, char[] pair) {
-        boolean[] checked = new boolean[goalArray.length];
-
-        int diffCount = 0;
-        int sameCount = 0;
-        for(int i = 0; i < goalArray.length; i++) {
-            boolean isExist = false;
-            for(int j = 0; j < pair.length; j++) {
-                if(goalArray[i] == pair[j]) {
-                    if(!checked[j]) {
-                        sameCount++;
-                        checked[j] = true;
-                        isExist = true;
-                        break;
-                    }
-                }
-            }
-            if(!isExist) {
-                diffCount++;
-            }
-        }
-        if(diffCount == 1) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean isSame(char[] goalArray, char[] pair) {
-        for(int i = 0; i < goalArray.length; i++) {
-            if(goalArray[i] != pair[i]) {
-                return false;
-            }
-        }
-        return true;
     }
 }
