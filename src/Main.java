2b998090ac4cb568c95d66c67704d6127c7d0f22
mikyeong Kwon <zxc_777@naver.com>
2026-02-20 07:46:31 +0900

    20260220

--- src/Main.java
+++ src/Main.java
@@ -2,99 +2,37 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int n, k, t, m;
+    static int max = Integer.MAX_VALUE;
+    static char[] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        graph = new char[input.length() * 2];
 
-        StringBuilder sb = new StringBuilder();
-        ///// pq.add(new int[]{i, teams[i], times[i], sum});
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> {
-            if(o1[3] == o2[3]) {
-                if(o1[1] == o2[1]) {
-                    return o1[2] - o2[2];
+        int aCount = 0;
+        for(int i =0 ; i < input.length(); i++) {
+            char c = input.charAt(i);
+            graph[i] = c;
+            graph[i + input.length()] = c;
+            if(c == 'a') aCount++;
+        }
+
+        int size = input.length();
+        for(int i = 0; i < graph.length - aCount; i++) {
+            int start = i;
+            int end = start + aCount;
+
+            int bCount = 0;
+            for(int j = start; j < end; j++) {
+                if(graph[j] == 'b') {
+                    bCount++;
                 }
-                return o1[1] - o2[1];
             }
-            return o2[3] - o1[3];
-        });
-
-        while (T-- > 0) {
-            pq.clear();
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
-            t = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-
-            int[] teams = new int[n + 1];
-            int[] times = new int[n + 1];
-            Info[] infos = new Info[k + 1];
-            for (int i = 0; i < infos.length; i++) {
-                infos[i] = new Info(n);
-            }
-
-            int counter = 1;
-            for (int i = 0; i < m; i++) {
-                StringTokenizer st1 = new StringTokenizer(br.readLine());
-                int team = Integer.parseInt(st1.nextToken());
-                int problem = Integer.parseInt(st1.nextToken());
-                int score = Integer.parseInt(st1.nextToken());
-
-                infos[problem].addScore(team, score);
-                teams[team]++;
-                times[team] = counter++;
-            }
-
-            for (int i = 1; i <= n; i++) {
-                int sum = 0;
-                for (Info info : infos) {
-                    sum += info.getScore(i);
-                }
-                pq.add(new int[]{i, teams[i], times[i], sum});
-            }
-
-            int rank = 1;
-            while(!pq.isEmpty()) {
-                int team = pq.poll()[0];
-                if(team == t) {
-                    sb.append(rank).append("\n");
-                    break;
-                }
-                rank++;
-            }
+            max = Math.min(max, bCount);
         }
-        System.out.println(sb);
+        System.out.println(max);
     }
 
-    private static boolean isAllGetted(boolean[] getted){
-        for (boolean result : getted) {
-            if (!result) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-class Info {
-    Map<Integer, Integer> map = new HashMap<>();
-
-    public Info(int n) {
-        for(int i = 1; i <= n; i++) {
-            map.put(i, 0);
-        }
-    }
-
-    public void addScore(int team, int score) {
-        int befo = map.get(team);
-        if(befo < score) {
-            map.put(team, score);
-        }
-    }
-    public int getScore(int team) {
-        return map.get(team);
-    }
 }
