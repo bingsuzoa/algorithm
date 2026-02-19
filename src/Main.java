73f641250c5cdd981b69bcc2a2083fb0576f6e5b
mikyeong Kwon <zxc_777@naver.com>
2026-02-19 19:32:23 +0900

    20260219

--- src/Main.java
+++ src/Main.java
@@ -2,60 +2,99 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[][] graph;
-    static int[][][] dp;
+    static int n, k, t, m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        graph = new int[N][M];
+        int T = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st1 = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++) {
-                graph[i][j] = Integer.parseInt(st1.nextToken());
-            }
-        }
-        dp = new int[N][M][3];
-        for(int i =0 ; i < dp.length; i++) {
-            for(int j = 0; j < dp[i].length; j++) {
-                Arrays.fill(dp[i][j], 1001);
-            }
-        }
-
-        for(int j = 0; j < M; j++) {
-            for(int k = 0; k < 3; k++) {
-                dp[0][j][k] = graph[0][j];
-            }
-        }
-
-        for(int i = 1; i < dp.length; i++) {
-            for(int j = 0; j < dp[i].length; j++) {
-                if(j - 1 >= 0) {
-                    dp[i][j][0] = Math.min(dp[i-1][j-1][1], dp[i][j][0]);
-                    dp[i][j][2] = Math.min(dp[i-1][j-1][1], dp[i][j][2]);
+        StringBuilder sb = new StringBuilder();
+        ///// pq.add(new int[]{i, teams[i], times[i], sum});
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> {
+            if(o1[3] == o2[3]) {
+                if(o1[1] == o2[1]) {
+                    return o1[2] - o2[2];
                 }
-                if(j + 1 < dp[0].length) {
-                    dp[i][j][0] = Math.min(dp[i-1][j+1][2], dp[i][j][0]);
-                    dp[i][j][1] = Math.min(dp[i-1][j+1][2], dp[i][j][1]);
-                }
-                dp[i][j][1] = Math.min(dp[i-1][j][0], dp[i][j][1]);
-                dp[i][j][2] = Math.min(dp[i-1][j][0], dp[i][j][2]);
+                return o1[1] - o2[1];
+            }
+            return o2[3] - o1[3];
+        });
 
-                dp[i][j][0] += graph[i][j];
-                dp[i][j][1] += graph[i][j];
-                dp[i][j][2] += graph[i][j];
+        while (T-- > 0) {
+            pq.clear();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            t = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+
+            int[] teams = new int[n + 1];
+            int[] times = new int[n + 1];
+            Info[] infos = new Info[k + 1];
+            for (int i = 0; i < infos.length; i++) {
+                infos[i] = new Info(n);
+            }
+
+            int counter = 1;
+            for (int i = 0; i < m; i++) {
+                StringTokenizer st1 = new StringTokenizer(br.readLine());
+                int team = Integer.parseInt(st1.nextToken());
+                int problem = Integer.parseInt(st1.nextToken());
+                int score = Integer.parseInt(st1.nextToken());
+
+                infos[problem].addScore(team, score);
+                teams[team]++;
+                times[team] = counter++;
+            }
+
+            for (int i = 1; i <= n; i++) {
+                int sum = 0;
+                for (Info info : infos) {
+                    sum += info.getScore(i);
+                }
+                pq.add(new int[]{i, teams[i], times[i], sum});
+            }
+
+            int rank = 1;
+            while(!pq.isEmpty()) {
+                int team = pq.poll()[0];
+                if(team == t) {
+                    sb.append(rank).append("\n");
+                    break;
+                }
+                rank++;
             }
         }
-        int answer = 1001;
-        for(int j = 0; j < dp[0].length; j++) {
-            for(int k = 0; k < 3; k++) {
-                answer = Math.min(answer, dp[N-1][j][k]);
+        System.out.println(sb);
+    }
+
+    private static boolean isAllGetted(boolean[] getted){
+        for (boolean result : getted) {
+            if (!result) {
+                return false;
             }
         }
-        System.out.println(answer);
+        return true;
+    }
+}
+
+class Info {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public Info(int n) {
+        for(int i = 1; i <= n; i++) {
+            map.put(i, 0);
+        }
+    }
+
+    public void addScore(int team, int score) {
+        int befo = map.get(team);
+        if(befo < score) {
+            map.put(team, score);
+        }
+    }
+    public int getScore(int team) {
+        return map.get(team);
     }
 }
