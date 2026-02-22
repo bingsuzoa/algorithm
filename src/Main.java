cabba6991aff54399c7c3c3b6a6e0d333b5ad96c
mikyeong Kwon <zxc_777@naver.com>
2026-02-23 08:03:40 +0900

    20260223

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,96 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static Set<Character> set;
+    static Word[] graph;
+    static StringBuilder answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        set = new HashSet<>();
+        answer = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
+        graph = new Word[N];
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> {
-            return o1 - o2;
-        });
+        for(int i =0 ; i < N; i++) {
+            graph[i] = new Word(br.readLine());
+        }
+
+        for(Word word : graph) {
+            if(isStep1(word)) continue;
+            if(isStep2(word)) continue;
+            answer.append(word.input).append("\n");
+        }
+        System.out.println(answer);
+    }
+    private static boolean isStep2(Word word) {
+        String[] words = word.words;
+
+        for(int i =0 ; i < words.length; i++) {
+            String w = words[i];
+            for(int j = 0; j < w.length(); j++) {
+                char c = Character.toUpperCase(w.charAt(j));
+                if(set.contains(c)) continue;
+                set.add(c);
+                makeResult(word, i, j);
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean isStep1(Word word) {
+        String[] words = word.words;
+
+        for(int i =0 ; i < words.length; i++) {
+            String w = words[i];
+            char c = Character.toUpperCase(w.charAt(0));
+            if(!set.contains(c)) {
+                set.add(c);
+                makeResult(word, i, 0);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void makeResult(Word word, int idx, int iidx) {
         StringBuilder sb = new StringBuilder();
 
-        while(N --> 0) {
-            int n = Integer.parseInt(br.readLine());
-
-            if(n == 0) {
-                if(pq.isEmpty()) {
-                    sb.append(0).append("\n");
-                } else {
-                    sb.append(pq.poll()).append("\n");
-                }
-                continue;
+        String[] words = word.words;
+        for(int i = 0; i < words.length; i++) {
+            String w = words[i];
+            if(i == idx) {
+                sb.append(getWord(w, iidx)).append(" ");
+            } else {
+                sb.append(w).append(" ");
             }
-            pq.add(n);
         }
-        System.out.println(sb);
+        answer.append(sb).append("\n");
+    }
+
+    private static String getWord(String w, int idx) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < w.length(); i++) {
+            if(i == idx) {
+                sb.append("[");
+                sb.append(w.charAt(i));
+                sb.append("]");
+            } else {
+                sb.append(w.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
+
+class Word {
+    String input;
+    String[] words;
+
+    public Word(String input) {
+        this.input = input;
+        words = input.split(" ");
     }
 }
