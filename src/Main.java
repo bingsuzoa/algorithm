0d9882a8b6b7b61691588f3f6a06f0c1739fde99
mikyeong Kwon <zxc_777@naver.com>
2026-02-23 07:29:00 +0900

    20260223

--- src/Main.java
+++ src/Main.java
@@ -6,49 +6,25 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
+        int N = Integer.parseInt(br.readLine());
 
-        int[] graph = new int[input.length()];
-
-        int zero = 0;
-        int one = 0;
-        int zeroIdx = 0;
-        int oneIdx = input.length() -1;
-        for(int i = 0; i < input.length(); i++) {
-            graph[i] = input.charAt(i)-'0';
-            if(graph[i] == 0) {
-                zero++;
-                zeroIdx = i;
-            }
-            if(graph[i] == 1) {
-                one++;
-                oneIdx = Math.min(oneIdx, i);
-            }
-        }
-        zero /= 2;
-        one /= 2;
-
-        for(int i = zeroIdx; i >= 0; i--) {
-            if(graph[i] == 0) {
-                if(zero <= 0) break;
-                graph[i] = -1;
-                zero--;
-            }
-        }
-
-        for(int i = oneIdx; i < graph.length; i++) {
-            if(graph[i] == 1) {
-                if(one <= 0) break;
-                graph[i] = -1;
-                one--;
-            }
-        }
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> {
+            return o1 - o2;
+        });
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < graph.length; i++) {
-            if(graph[i] != -1) {
-                sb.append(graph[i]);
+
+        while(N --> 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            if(n == 0) {
+                if(pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+                continue;
             }
+            pq.add(n);
         }
         System.out.println(sb);
     }
