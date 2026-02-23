d09a0b010229c0c5a7970a139b4022ccb64269bb
mikyeong Kwon <zxc_777@naver.com>
2026-02-24 07:18:33 +0900

    20260224

--- src/Main.java
+++ src/Main.java
@@ -2,96 +2,38 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static Set<Character> set;
-    static Word[] graph;
-    static StringBuilder answer;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        set = new HashSet<>();
-        answer = new StringBuilder();
+
+        dp = new int[10001][4];
 
         int N = Integer.parseInt(br.readLine());
-        graph = new Word[N];
 
-        for(int i =0 ; i < N; i++) {
-            graph[i] = new Word(br.readLine());
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][2] = 1;
+        dp[3][1] = 1;
+        dp[3][3] = 1;
+
+        for(int i = 4; i < dp.length; i++) {
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1] + dp[i-2][2];
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
         }
-
-        for(Word word : graph) {
-            if(isStep1(word)) continue;
-            if(isStep2(word)) continue;
-            answer.append(word.input).append("\n");
-        }
-        System.out.println(answer);
-    }
-    private static boolean isStep2(Word word) {
-        String[] words = word.words;
-
-        for(int i =0 ; i < words.length; i++) {
-            String w = words[i];
-            for(int j = 0; j < w.length(); j++) {
-                char c = Character.toUpperCase(w.charAt(j));
-                if(set.contains(c)) continue;
-                set.add(c);
-                makeResult(word, i, j);
-                return true;
-            }
-        }
-        return false;
-    }
-    private static boolean isStep1(Word word) {
-        String[] words = word.words;
-
-        for(int i =0 ; i < words.length; i++) {
-            String w = words[i];
-            char c = Character.toUpperCase(w.charAt(0));
-            if(!set.contains(c)) {
-                set.add(c);
-                makeResult(word, i, 0);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static void makeResult(Word word, int idx, int iidx) {
         StringBuilder sb = new StringBuilder();
 
-        String[] words = word.words;
-        for(int i = 0; i < words.length; i++) {
-            String w = words[i];
-            if(i == idx) {
-                sb.append(getWord(w, iidx)).append(" ");
-            } else {
-                sb.append(w).append(" ");
+        while(N --> 0) {
+            int n = Integer.parseInt(br.readLine());
+            int sum = 0;
+            for(int j = 0; j < dp[0].length; j++) {
+                sum += dp[n][j];
             }
+            sb.append(sum).append("\n");
         }
-        answer.append(sb).append("\n");
+        System.out.println(sb);
     }
 
-    private static String getWord(String w, int idx) {
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < w.length(); i++) {
-            if(i == idx) {
-                sb.append("[");
-                sb.append(w.charAt(i));
-                sb.append("]");
-            } else {
-                sb.append(w.charAt(i));
-            }
-        }
-        return sb.toString();
-    }
-}
-
-class Word {
-    String input;
-    String[] words;
-
-    public Word(String input) {
-        this.input = input;
-        words = input.split(" ");
-    }
 }
