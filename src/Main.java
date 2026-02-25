e64b0088db87eca3b96decc9e7c2d90c3dbb1f9d
mikyeong Kwon <zxc_777@naver.com>
2026-02-26 08:06:22 +0900

    20260226

--- src/Main.java
+++ src/Main.java
@@ -2,85 +2,71 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[] belts;
-    static boolean[] boxs;
-    static int N,K;
-    static int MOD = Integer.MAX_VALUE;
+    static int N, M;
+    static Map<Integer, Integer> stairs;
+    static Map<Integer, Integer> snakes;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        stairs = new HashMap<>();
+        snakes = new HashMap<>();
 
-        belts = new int[N * 2];
-        boxs = new boolean[N * 2];
-
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        for(int i = 0; i < belts.length; i++) {
-            belts[i] = Integer.parseInt(st1.nextToken());
+        int max = 101;
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st1.nextToken());
+            int end = Integer.parseInt(st1.nextToken());
+            max = Math.max(max, Math.max(start, end));
+            stairs.put(start, end);
         }
 
-        int count = 1;
-        while(true) {
-            rotateBelt();
-            rotateRobot();
-            upRobot();
-            if(isEnd()) {
-                break;
+        for(int i = 0; i < M; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st1.nextToken());
+            int end = Integer.parseInt(st1.nextToken());
+            max = Math.max(max, Math.max(start, end));
+            snakes.put(start, end);
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> {
+            return o1[1] - o2[1];
+        });
+
+        pq.add(new int[]{1, 0});
+        int[] dist = new int[max];
+        Arrays.fill(dist, max + 1);
+        dist[1] = 0;
+
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int pos = cur[0];
+            int count = cur[1];
+
+            if(pos == 100) {
+                System.out.println(count);
+                return;
             }
-            count++;
-        }
 
-        System.out.println(count);
-    }
-    private static boolean isEnd() {
-        int count = 0;
-        for(int remain : belts) {
-            if(remain == 0) count++;
-        }
-        if(count >= K) {
-            return true;
-        }
-        return false;
-    }
-    private static void upRobot() {
-        if(boxs[0]) return;
-        if(belts[0] < 1) return;
-        belts[0] --;
-        boxs[0] = true;
-    }
-    private static void rotateRobot() {
-        for(int i = N - 2; i >= 0; i--) {
-            if(!boxs[i]) continue;
-            if(boxs[i+1]) continue;
-            if(belts[i+1] < 1) continue;
+            for(int i = 1; i <= 6; i++) {
+                int next = pos + i;
+                if(next > 100) break;
 
-            boxs[i+1] = true;
-            belts[i+1] --;
-            boxs[i] = false;
-
-            if(i+1 == N-1 && boxs[i+1]) {
-                boxs[i+1] = false;
-            }
-        }
-    }
-
-
-    private static void rotateBelt() {
-        int lastRemain = belts[belts.length - 1];
-        boolean lastBox = boxs[boxs.length -1];
-        for(int i = belts.length -1; i >= 1; i--) {
-            belts[i] = belts[i-1];
-            boxs[i] = boxs[i-1];
-
-            if(i == N-1) {
-                boxs[i] = false;
+                while(stairs.containsKey(next)) {
+                    next = stairs.get(next);
+                }
+                while(snakes.containsKey(next)) {
+                    next = snakes.get(next);
+                }
+                if(dist[next] > count + 1) {
+                    dist[next] = count + 1;
+                    pq.add(new int[]{next, count + 1});
+                }
             }
         }
-        belts[0] = lastRemain;
-        boxs[0] = lastBox;
     }
-
 }
