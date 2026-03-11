5b0610e20e75f47ad4c3602115a8b39bcec4cd83
mikyeong Kwon <zxc_777@naver.com>
2026-03-12 08:48:30 +0900

    20260312

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,65 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static char[] graph;
+    static int N;
+    static String input;
+    static String goal;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
+        N = Integer.parseInt(br.readLine());
+        input = br.readLine();
+        goal = br.readLine();
 
-        graph = new char[input.length() * 2];
-
-        int aCount = 0;
-        for(int i = 0; i < input.length(); i++) {
-            graph[i] = input.charAt(i);
-            if(graph[i] == 'a') {
-                aCount++;
-            }
-            graph[i + input.length()] = input.charAt(i);
+        if(input.equals(goal)) {
+            System.out.println(0);
+            return;
         }
 
-        int left = 0;
-        int right = aCount - 1;
-        int aMin = 0;
-        for(int i = 0; i <= right; i++) {
-            if(graph[i] == 'b') {
-                aMin++;
+        int[] graph = new int[N];
+        for(int i=0 ;i < graph.length; i++) {
+            graph[i] = input.charAt(i) - '0';
+        }
+
+        int[] original = Arrays.copyOf(graph, graph.length);
+        int res1 = check(original);
+
+        int[] pair = Arrays.copyOf(graph, graph.length);
+        pair[0] = graph[0] == 0 ? 1 : 0;
+        pair[1] = graph[1] == 0 ? 1 : 0;
+        int res2 = check(pair) + 1;
+
+        int answer = Integer.MAX_VALUE;
+        if(original[N-1] == goal.charAt(N-1) - '0') {
+            answer = Math.min(answer, res1);
+        }
+        if(pair[N-1] == goal.charAt(N-1) - '0') {
+            answer = Math.min(answer, res2);
+        }
+        if(answer == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(answer);
+        }
+    }
+    private static int check(int[] graph) {
+        int count = 0;
+        for(int i = 1 ; i < input.length(); i++) {
+            int bi = graph[i-1];
+            int bg = goal.charAt(i-1) - '0';
+
+            if(bi != bg) {
+                count++;
+                int v = bi == 0 ? 1 : 0;
+
+                graph[i-1] = v;
+                graph[i] = graph[i] == 0 ? 1 : 0;
+                if(i + 1 < input.length()) {
+                    graph[i+1] = graph[i+1] == 0 ? 1 : 0;
+                }
             }
         }
-        int answer = aMin;
-        while(true) {
-            char lc = graph[left];
-            left++;
-            right++;
-            if(right >= graph.length || left >= input.length()) break;
-            char rc = graph[right];
-            if(lc == 'b') {
-                aMin --;
-            }
-
-            if(rc == 'b') {
-                aMin++;
-            }
-
-            answer = Math.min(answer, aMin);
-        }
-        System.out.println(answer);
+        return count;
     }
 }
