7a688f7769d2ca10e7cf0ca3b196129dd22a4fc1
mikyeong Kwon <zxc_777@naver.com>
2026-03-16 07:42:21 +0900

    20260316

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,48 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    static int[][] dp;
+    static int[] graph;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] dp = new int[12];
-        dp[1] = 1;
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 2; i < dp.length; i++) {
-            for(int j = 1; j <= 3; j++) {
-                if(i - j >= 1) {
-                    dp[i] += dp[i-j];
-                }
-            }
-            if(i <= 3) {
-                dp[i]++;
-            }
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N + 1];
+        for(int i = 1; i < graph.length; i++) {
+            graph[i] = Integer.parseInt(br.readLine());
         }
-        while(N --> 0) {
-            int n = Integer.parseInt(br.readLine());
-            sb.append(dp[n]).append("\n");
+
+        dp = new int[N + 1][3];
+        for(int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -10000);
         }
-        System.out.println(sb);
+
+        dfs(0, 0);
+        int max  = -100000;
+        for(int value : dp[0]) {
+            max = Math.max(value, max);
+        }
+        System.out.println(max);
+    }
+
+    private static int dfs(int cur, int count) {
+        if(cur >= graph.length) {
+            return -10000;
+        }
+        if(dp[cur][count] != -10000) {
+            return dp[cur][count];
+        }
+        if(cur == N) {
+            return graph[N];
+        }
+        if(count == 2) {
+            dp[cur][count] = Math.max(dp[cur][count], dfs(cur + 2, 1) + graph[cur]);
+        } else {
+            dp[cur][count] = Math.max(dp[cur][count], dfs(cur + 1, count + 1) + graph[cur]);
+            dp[cur][count] = Math.max(dp[cur][count], dfs(cur + 2, 1) + graph[cur]);
+        }
+        return dp[cur][count];
     }
 }
