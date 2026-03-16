0c6b0bb1ac3e27313fe12bc7e71a09abad2a8738
mikyeong Kwon <zxc_777@naver.com>
2026-03-17 07:30:16 +0900

    20260317

--- src/Main.java
+++ src/Main.java
@@ -2,53 +2,41 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int N;
-    static Stack<Integer> stack;
+    static int[][] graph;
+    static int[][] dp;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        stack = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+        graph = new int[N][3];
 
-        int count = 0;
-        for(int i = 0; i < N; i++) {
+        for(int i =0 ;i < graph.length; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            if(y == 0) {
-                stack.clear();
-                continue;
+            for(int j = 0; j < graph[i].length; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
-
-            if(stack.isEmpty()) {
-                stack.push(y);
-                count++;
-                continue;
-            }
-
-            while(!stack.isEmpty()) {
-                int befo = stack.peek();
-                if(befo < y) {
-                    count++;
-                    stack.push(y);
-                    break;
-                }
-                else if(befo > y) {
-                    stack.pop();
-                }
-                else {
-                    break;
-                }
-            }
-            if(stack.isEmpty()) {
-                count++;
-                stack.push(y);
-            }
-
         }
 
-        System.out.println(count);
+        dp = new int[N][3];
+        for(int i =0 ; i < dp.length; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        int answer = Math.min(dfs(0,0), Math.min(dfs(0,1), dfs(0,2)));
+        System.out.println(answer);
+    }
+
+    private static int dfs(int cur, int color) {
+        if(dp[cur][color] != Integer.MAX_VALUE) {
+            return dp[cur][color];
+        }
+        if(cur == dp.length -1) {
+            return graph[cur][color];
+        }
+        for(int next = 0; next < 3; next++) {
+            if(next == color) continue;
+            dp[cur][color] = Math.min(dp[cur][color], dfs(cur + 1, next) + graph[cur][color]);
+        }
+        return dp[cur][color];
     }
 }
