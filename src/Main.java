ee4d89c704dffa26c770ecb7a498d8ec81e1a7fc
mikyeong Kwon <zxc_777@naver.com>
2026-03-18 07:12:02 +0900

    20260318

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,34 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[][] graph;
-    static int[][] dp;
+    static int N;
+    static int[] dp;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        graph = new int[N][3];
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
 
-        for(int i =0 ;i < graph.length; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < graph[i].length; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-            }
+        if(N == 1) {
+            dp[1] = 1;
+            System.out.println(1);
+            return;
+        }
+        if(N == 2) {
+            dp[1] = 1;
+            dp[2] = 2;
+            System.out.println(2);
+            return;
         }
 
-        dp = new int[N][3];
-        for(int i =0 ; i < dp.length; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i < dp.length; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
         }
-        int answer = Math.min(dfs(0,0), Math.min(dfs(0,1), dfs(0,2)));
-        System.out.println(answer);
+
+        System.out.println(dp[N] % 10007);
     }
 
-    private static int dfs(int cur, int color) {
-        if(dp[cur][color] != Integer.MAX_VALUE) {
-            return dp[cur][color];
-        }
-        if(cur == dp.length -1) {
-            return graph[cur][color];
-        }
-        for(int next = 0; next < 3; next++) {
-            if(next == color) continue;
-            dp[cur][color] = Math.min(dp[cur][color], dfs(cur + 1, next) + graph[cur][color]);
-        }
-        return dp[cur][color];
-    }
 }
