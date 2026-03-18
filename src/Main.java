ca60e8959aca7a3d8b392ecfd897ed4fae300515
mikyeong Kwon <zxc_777@naver.com>
2026-03-19 08:44:44 +0900

    20260319

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static char[][] graph;
+    static int[][] graph;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0,0, 1, -1};
     static int max = 0;
-    static Map<Character, Integer> map;
-    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,38 +13,35 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        graph = new char[N][M];
-        visited = new boolean[26];
-
-        map = new HashMap<>();
-        int idx = 0;
-        for(char i = 'A'; i <= 'Z'; i++) {
-            map.put(i, idx++);
-        }
+        graph = new int[N][M];
 
         for(int i = 0; i < graph.length; i++) {
             String input = br.readLine();
             for(int j = 0; j < graph[i].length; j++) {
-                graph[i][j] = input.charAt(j);
+                graph[i][j] = input.charAt(j) - 'A';
             }
         }
-        visited[map.get(graph[0][0])] = true;
-        dfs(0, 0, 1);
+
+        int startbit = 1 << graph[0][0];
+        dfs(startbit, 0, 0, 1);
         System.out.println(max);
 
     }
 
-    private static void dfs(int sx, int sy, int count) {
-
-        for(int i =0 ; i < 4; i++) {
+    private static void dfs(int bit, int sx, int sy, int count) {
+        for(int i = 0; i < 4; i++) {
             int nx = sx + dx[i];
             int ny = sy + dy[i];
             if(nx >= graph.length || nx < 0 || ny >= graph[0].length || ny < 0) continue;
-            if(visited[map.get(graph[nx][ny])]) continue;
-            visited[map.get(graph[nx][ny])] = true;
-            dfs(nx, ny, count + 1);
-            visited[map.get(graph[nx][ny])] = false;
+
+            int next = 1 << graph[nx][ny];
+            if((bit & next) != 0) {
+                continue;
+            }
+            dfs(bit | next, nx, ny, count + 1);
         }
+
         max = Math.max(max, count);
     }
+
 }
