3dac78404355131b6934d46409b7cd8990816e44
mikyeong Kwon <zxc_777@naver.com>
2026-03-19 08:00:39 +0900

    20260319

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,43 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int N,M;
-    static int[] graph;
-    static int[] prefix;
-    static StringBuilder sb = new StringBuilder();
+    static char[][] graph;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0,0, 1, -1};
+    static int max = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer input = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(input.nextToken());
-        M = Integer.parseInt(input.nextToken());
-
-        graph = new int[N + 1];
-        prefix = new int[N+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        graph = new char[N][M];
 
-        for(int i = 1;i < graph.length; i++) {
-            graph[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < graph.length; i++) {
+            String input = br.readLine();
+            for(int j = 0; j < graph[i].length; j++) {
+                graph[i][j] = input.charAt(j);
+            }
+        }
+        dfs(0, 0, "", 1);
+        System.out.println(max);
+
+    }
+
+    private static void dfs(int sx, int sy, String sb, int count) {
+        sb += String.valueOf(graph[sx][sy]);
+
+        for(int i = 0; i < 4; i++) {
+            int nx = sx + dx[i];
+            int ny = sy + dy[i];
+            if(nx >= graph.length || nx < 0 || ny >= graph[0].length || ny < 0) continue;
+            char next = graph[nx][ny];
+
+            if(sb.contains(String.valueOf(next))) continue;
+            dfs(nx, ny, sb + String.valueOf(next), count + 1);
         }
 
-        prefix[1] = graph[1];
-        for(int i = 2; i < prefix.length; i++) {
-            prefix[i] = prefix[i-1] + graph[i];
-        }
-
-        while(M --> 0) {
-            StringTokenizer st1 = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st1.nextToken());
-            int m = Integer.parseInt(st1.nextToken());
-
-            sb.append(prefix[m] - prefix[n-1]).append("\n");
-        }
-        System.out.println(sb);
+        max = Math.max(max, count);
     }
 }
