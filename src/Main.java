f7e0366856c2e48724aa62fce9d06ddca6dd2ff7
mikyeong Kwon <zxc_777@naver.com>
2026-03-23 08:34:32 +0900

    20260323

--- src/Main.java
+++ src/Main.java
@@ -2,60 +2,93 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int N;
-    static int[] dist;
-    static int[] memori;
-    static int MOD = Integer.MAX_VALUE;
+    static char[][] graph;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+        graph = new char[3][3];
+        sb = new StringBuilder();
 
-        dist = new int[N+1];
-        memori = new int[N+1];
-        Arrays.fill(dist, MOD);
+        while(true) {
+            String input = br.readLine();
+            if(input.equals("end")) break;
 
-        Queue<int[]> queue = new LinkedList<>();
-        memori[N] = N;
-        dist[N] = 0;
-        queue.add(new int[]{N, 0});
+            //절대 불가
+            //O의 개수가 더 많다.
+            //X의 개수가 2개 이상 많다.
+            int ocount = 0;
+            int xcount = 0;
+            for(char c : input.toCharArray()) {
+                if(c == 'O') ocount ++;
+                if(c == 'X') xcount ++;
+            }
+            if(ocount > xcount || xcount >= ocount + 2) {
+                sb.append("invalid").append("\n");
+                continue;
+            }
 
-        while(!queue.isEmpty()) {
-            int[] tmp = queue.poll();
-            int cur = tmp[0];
-            int count = tmp[1];
+            for(int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                int n = i/3;
+                int m = i%3;
+                graph[n][m] = c;
+            }
 
-            if(cur == 1) break;
-
-            int[] next = new int[3];
-            next[0] = cur % 3 == 0 ? cur / 3  : 0;
-            next[1] = cur % 2 == 0 ? cur / 2 : 0;
-            next[2] = cur - 1;
-
-            for(int i =0 ; i < next.length; i++) {
-                if(next[i] == 0) continue;
-                if(dist[next[i]] == MOD) {
-                    dist[next[i]] = count + 1;
-                    memori[next[i]] = cur;
-                    queue.add(new int[]{next[i], count + 1});
+            //X가 이겼을 때
+            //X의 갯수가 하나 더 많고
+            //가로/세로/대각선 방향으로 이었을 때
+            if(xcount == ocount + 1) {
+                if(isPass('X') && !isPass('O')) {
+                    sb.append("valid").append("\n");
+                    continue;
+                }
+                //비겼을 때
+                //게임판이 가득 찼을 때
+                if(xcount == 5 && ocount == 4 && !isPass('X') && !isPass('O')) {
+                    sb.append("valid").append("\n");
+                    continue;
                 }
             }
-        }
 
-        Stack<Integer> stack = new Stack<>();
-        int idx = 1;
-        stack.push(idx);
-        while(idx < N) {
-            stack.push(memori[idx]);
-            idx = memori[idx];
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(stack.size() -1).append("\n");
-        while(!stack.isEmpty()) {
-            sb.append(stack.pop()).append(" ");
+            //O이 이겼을 때
+            //갯수 동일
+            if(xcount == ocount) {
+                if(isPass('O') && !isPass('X')) {
+                    sb.append("valid").append("\n");
+                    continue;
+                }
+            }
+            sb.append("invalid").append("\n");
         }
         System.out.println(sb);
+    }
+    private static boolean isPass(char c) {
+        if(graph[0][0] == graph[1][0] && graph[1][0] == graph[2][0] && graph[2][0] == c) {
+            return true;
+        }
+        if(graph[0][1] == graph[1][1] && graph[1][1] == graph[2][1] && graph[2][1] == c) {
+            return true;
+        }
+        if(graph[0][2] == graph[1][2] && graph[1][2] == graph[2][2] && graph[2][2] == c) {
+            return true;
+        }
+        if(graph[0][0] == graph[1][1] && graph[1][1] == graph[2][2] && graph[2][2] == c) {
+            return true;
+        }
+        if(graph[0][2] == graph[1][1] && graph[1][1] == graph[2][0] && graph[2][0] == c) {
+            return true;
+        }
+        if(graph[0][0] == graph[0][1] && graph[0][1] == graph[0][2] && graph[0][2] == c) {
+            return true;
+        }
+        if(graph[1][0] == graph[1][1] && graph[1][1] == graph[1][2] && graph[1][2] == c) {
+            return true;
+        }
+        if(graph[2][0] == graph[2][1] && graph[2][1] == graph[2][2] && graph[2][2] == c) {
+            return true;
+        }
+        return false;
     }
 }
