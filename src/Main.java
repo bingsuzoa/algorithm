d3e6596954940e707d9d327e3137cacce03c5ef6
mikyeong Kwon <zxc_777@naver.com>
2026-03-27 08:42:19 +0900

    20260327

--- src/Main.java
+++ src/Main.java
@@ -1,84 +1,33 @@
 import java.util.*;
 import java.io.*;
 
+
 class Main {
-    static int N;
-    static Map<String, Integer> map;
-    static String[] graph;
-    static String[] result;
-    static int max = -1;
+    static int[] dp;
+    static int[] graph;
+    static int max = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        map = new HashMap<>();
-        graph = new String[N];
-        result = new String[2];
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N];
+        graph = new int[N];
 
-
-
-        for(int i =0 ;i < N; i++) {
-            graph[i] = br.readLine();
+        for(int i = 0; i < N; i++) {
+            graph[i] = Integer.parseInt(br.readLine());
         }
 
-        for (int i = 0; i < N; i++) {
-            map.putIfAbsent(graph[i], i);
-        }
-        Arrays.sort(graph);
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < N - 1; i++) {
-            for(int j = i + 1; j < N; j++) {
-                String left;
-                String right;
-
-                left = map.get(graph[i]) < map.get(graph[j]) ? graph[i] : graph[j];
-                right = map.get(graph[i]) < map.get(graph[j]) ? graph[j] : graph[i];
-
-                if(left.equals(right)) continue;
-                if(left.charAt(0) != right.charAt(0)) break;
-                int min = Math.min(left.length(), right.length());
-                sb.setLength(0);
-
-                int count = 0;
-                for (int k = 0; k < min; k++) {
-                    if (left.charAt(k) == right.charAt(k)) {
-                        sb.append(left.charAt(k));
-                        count++;
-                    } else {
-                        break;
-                    }
+        for(int i = 0;i < graph.length; i++) {
+            dp[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(graph[j] < graph[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-
-                if (max < count) {
-                    max = count;
-                    result[0] = left;
-                    result[1] = right;
-
-                } else if (max == count) {
-                    int resultLeftIdx = map.get(result[0]);
-                    int resultRightIdx = map.get(result[1]);
-                    int leftIdx = map.get(left);
-                    int rightIdx = map.get(right);
-
-                    if (leftIdx < resultLeftIdx) {
-                        result[0] = left;
-                        result[1] = right;
-                    } else if (leftIdx == resultLeftIdx && rightIdx < resultRightIdx) {
-                        if (leftIdx < rightIdx) {
-                            result[0] = left;
-                            result[1] = right;
-                        } else {
-                            result[0] = right;
-                            result[1] = left;
-                        }
-                    }
-                }
-
             }
+            max = Math.max(max, dp[i]);
         }
-
-        System.out.println(result[0] + "\n" + result[1]);
+        System.out.println(N - max);
     }
+
 }
