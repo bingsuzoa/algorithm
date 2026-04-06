00f5ad9953e87e0a06932d79210119cadc05fc10
mikyeong Kwon <zxc_777@naver.com>
2026-04-07 08:53:30 +0900

    20260407

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,43 @@
 import java.util.*;
 import java.io.*;
 
+
 class Main {
-    static int[] graph;
-    static int[][] dp;
+    static String input, goal;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        input = br.readLine();
+        goal = br.readLine();
 
-        graph = new int[N + 1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        if(dfs(goal)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
 
-        for(int i = 1; i <= N; i++) {
-            graph[i] = Integer.parseInt(st.nextToken());
+    private static boolean dfs(String tmp) {
+        boolean isPossible = false;
+
+        if(tmp.length() == input.length()) {
+            if(tmp.equals(input)) {
+                return true;
+            }
+            return false;
         }
 
-        dp = new int[N + 1][3];
-
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 1; i <= N; i++) {
-
-            while(!stack.isEmpty() && graph[stack.peek()] <= graph[i]) {
-                stack.pop();
-            }
-            dp[i][2] += stack.size();
-            if(!stack.isEmpty()) {
-                dp[i][1] = stack.peek();
-            }
-            stack.push(i);
+        char c = tmp.charAt(tmp.length() -1);
+        char s = tmp.charAt(0);
+        if(c == 'A') {
+            isPossible |= dfs(tmp.substring(0, tmp.length() -1));
         }
-
-        stack.clear();
-
-        for(int i = N; i>= 1; i--) {
-
-            while(!stack.isEmpty() && graph[stack.peek()] <= graph[i]) {
-                stack.pop();
-            }
-            dp[i][2] += stack.size();
-            if(!stack.isEmpty()) {
-                int tmp =  stack.peek();
-                if(dp[i][1] == 0 || Math.abs(i - tmp) < Math.abs(dp[i][1] - i)) {
-                    dp[i][1] = tmp;
-                }
-                else if(Math.abs(i - tmp) == Math.abs(dp[i][1] - i)) {
-                    if(tmp < dp[i][1]) {
-                        dp[i][1] = tmp;
-                    }
-                }
-
-            }
-            stack.push(i);
+        if(s == 'B') {
+            StringBuilder sb = new StringBuilder();
+            sb.append(tmp).reverse();
+            isPossible |= dfs(sb.toString().substring(0, tmp.length() -1));
         }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= N; i++) {
-            if(dp[i][2] == 0) {
-                sb.append("0").append("\n");
-            } else {
-                sb.append(dp[i][2] + " " + dp[i][1]).append("\n");
-            }
-        }
-        System.out.println(sb);
+        return isPossible;
     }
 }
