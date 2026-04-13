b662792f87e100f80edbed5e39bae8bf4c4ce5d8
mikyeong Kwon <zxc_777@naver.com>
2026-04-13 09:03:17 +0900

    20260413

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,36 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[] graph;
+    static int C, N;
     static int[] dp;
-    static StringBuilder sb = new StringBuilder();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        while(N --> 0) {
-            int n = Integer.parseInt(br.readLine());
-            graph = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        C = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < graph.length; i++) {
-                graph[i] = Integer.parseInt(st.nextToken());
+        dp = new int[C + 100];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for(int i = 0 ; i < N; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int cost = Integer.parseInt(st1.nextToken());
+            int value = Integer.parseInt(st1.nextToken());
+
+            for(int j = value; j < dp.length; j++) {
+                if(dp[j-value] == Integer.MAX_VALUE)  continue;
+                    dp[j] = Math.min(dp[j], dp[j - value] + cost);
             }
-
-            int goal = Integer.parseInt(br.readLine());
-            dp = new int[goal + 1];
-            dp[0] = 1;
-
-            for(int coin : graph) {
-                for(int i = coin; i <= goal; i++) {
-                    dp[i] += dp[i - coin];
-                }
-            }
-            sb.append(dp[goal]).append("\n");
         }
-        System.out.println(sb);
+
+        int max = Integer.MAX_VALUE;
+        for(int i = C; i < dp.length; i++) {
+            max = Math.min(max, dp[i]);
+        }
+        System.out.println(max);
     }
 }
