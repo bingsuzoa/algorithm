e3b2e2aba91fd412f0a8180b036797532075ba2b
mikyeong Kwon <zxc_777@naver.com>
2026-04-14 08:15:43 +0900

    20260414

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,51 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int C, N;
-    static int[] dp;
-
+    static int[] graph;
+    static boolean[] coins;
+    static List<int[]> list;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        C = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+        int N = 3;
+        while(N --> 0) {
+            int n = Integer.parseInt(br.readLine());
+            list = new ArrayList<>();
 
-        dp = new int[C + 100];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+            int goal = 0;
+            while(n --> 0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int coin = Integer.parseInt(st.nextToken());
+                int count = Integer.parseInt(st.nextToken());
+                list.add(new int[]{coin, count});
+                goal += (coin * count);
+            }
+            graph = new int[goal / 2 + 1];
+            coins = new boolean[goal / 2 + 1];
+            coins[0] = true;
 
-        for(int i = 0 ; i < N; i++) {
-            StringTokenizer st1 = new StringTokenizer(br.readLine());
-            int cost = Integer.parseInt(st1.nextToken());
-            int value = Integer.parseInt(st1.nextToken());
+            for(int[] cur : list) {
+                int coin = cur[0];
+                int count = cur[1];
 
-            for(int j = value; j < dp.length; j++) {
-                if(dp[j-value] == Integer.MAX_VALUE)  continue;
-                    dp[j] = Math.min(dp[j], dp[j - value] + cost);
+                Arrays.fill(graph, 0);
+
+                for(int i = coin; i < graph.length; i++) {
+                    if(coins[i]) continue;
+                    if(!coins[i - coin] || graph[i-coin] + 1 > count) continue;
+                    graph[i] = graph[i - coin] + 1;
+                    coins[i] = true;
+                }
+            }
+
+            if(coins[graph.length -1]) {
+                sb.append("1").append("\n");
+            } else {
+                sb.append("0").append("\n");
             }
         }
-
-        int max = Integer.MAX_VALUE;
-        for(int i = C; i < dp.length; i++) {
-            max = Math.min(max, dp[i]);
-        }
-        System.out.println(max);
+        System.out.println(sb);
     }
 }
