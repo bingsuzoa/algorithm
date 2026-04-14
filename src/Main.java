e52350c810cc6e57cb7bb02f70e9876ecebc1e8c
mikyeong Kwon <zxc_777@naver.com>
2026-04-15 07:20:40 +0900

    20260415

--- src/Main.java
+++ src/Main.java
@@ -2,51 +2,35 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[] graph;
-    static boolean[] coins;
-    static List<int[]> list;
-    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = 3;
-        while(N --> 0) {
-            int n = Integer.parseInt(br.readLine());
-            list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int goal = 0;
-            while(n --> 0) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int coin = Integer.parseInt(st.nextToken());
-                int count = Integer.parseInt(st.nextToken());
-                list.add(new int[]{coin, count});
-                goal += (coin * count);
-            }
-            graph = new int[goal / 2 + 1];
-            coins = new boolean[goal / 2 + 1];
-            coins[0] = true;
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
 
-            for(int[] cur : list) {
-                int coin = cur[0];
-                int count = cur[1];
+        System.out.println(check(B) - check(A - 1));
+    }
 
-                Arrays.fill(graph, 0);
+    private static long check(long num) {
+        long count = 0;
+        long totalNumbers = num + 1;
 
-                for(int i = coin; i < graph.length; i++) {
-                    if(coins[i]) continue;
-                    if(!coins[i - coin] || graph[i-coin] + 1 > count) continue;
-                    graph[i] = graph[i - coin] + 1;
-                    coins[i] = true;
-                }
-            }
+        for(int i = 0; (1L << i) <= num; i++) {
+            long cycle = 1L << (i+1);
+            long halfCycle = 1L << (i);
 
-            if(coins[graph.length -1]) {
-                sb.append("1").append("\n");
-            } else {
-                sb.append("0").append("\n");
+            count += (totalNumbers / cycle) * halfCycle;
+
+            long remainder = totalNumbers % cycle;
+            if(remainder > halfCycle) {
+                count += (remainder - halfCycle);
             }
         }
-        System.out.println(sb);
+        return count;
     }
+
+
 }
