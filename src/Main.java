ed342ce67b6c5ef7ece128a767a4914c6faca9ca
mikyeong Kwon <zxc_777@naver.com>
2026-04-20 08:25:22 +0900

    20260420

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,64 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int N, K;
-    static PriorityQueue<int[]> pq;
-    static PriorityQueue<Long> weights;
-    static long answer = 0;
+    static int[][] graph;
+    static int[][][] dp;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        pq = new PriorityQueue<>((o1,o2) -> {
-            return o1[0] - o2[0];
-        });
+        graph = new int[N][M];
+        dp = new int[N][M][2];
 
-        while(N --> 0) {
+        for(int i = 0;i < graph.length; i++) {
             StringTokenizer st1 = new StringTokenizer(br.readLine());
-            int M = Integer.parseInt(st1.nextToken());
-            int V = Integer.parseInt(st1.nextToken());
-            pq.add(new int[]{M,V});
-        }
-
-        weights = new PriorityQueue<>((o1,o2) -> {
-            return o1.compareTo(o2);
-        });
-
-        while(K --> 0) {
-            weights.add(Long.parseLong(br.readLine()));
-        }
-
-        PriorityQueue<int[]> tmp = new PriorityQueue<>((o1,o2) -> {
-            if(o1[1] == o2[1]) {
-                return o1[0] - o2[0];
-            }
-            return o2[1] - o1[1];
-        });
-        while(!weights.isEmpty()) {
-            long bagWeight = weights.poll();
-
-            while(!pq.isEmpty() && pq.peek()[0] <= bagWeight) {
-                tmp.add(pq.poll());
-            }
-
-            if(!tmp.isEmpty()) {
-                answer += tmp.poll()[1];
+            for(int j = 0; j < graph[0].length; j++) {
+                graph[i][j] = Integer.parseInt(st1.nextToken());
             }
         }
-        System.out.println(answer);
+
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp[i].length; j++) {
+                Arrays.fill(dp[i][j], -1000001);
+            }
+        }
+        dp[0][0][0] = graph[0][0];
+        dp[0][0][1] = graph[0][0];
+
+        for(int j = 1; j < graph[0].length; j++) {
+            dp[0][j][0] = dp[0][j-1][0] + graph[0][j];
+            dp[0][j][1] = dp[0][j][0];
+        }
+
+        for(int i = 1; i < graph.length; i++) {
+            //left
+            for(int j = 0; j < graph[0].length; j++) {
+                int up = Math.max(dp[i-1][j][0], dp[i-1][j][1]);
+                if(j == 0) {
+                    dp[i][j][0] = Math.max(dp[i][j][0], up + graph[i][j]);
+                } else {
+                    dp[i][j][0] = Math.max(dp[i][j][0], up + graph[i][j]);
+                    dp[i][j][0] = Math.max(dp[i][j][0], dp[i][j-1][0]  + graph[i][j]);
+                }
+            }
+
+            //right
+            for(int j = graph[0].length -1; j >= 0; j--) {
+                int up = Math.max(dp[i-1][j][0], dp[i-1][j][1]);
+
+                if(j == graph[0].length -1) {
+                    dp[i][j][1] = Math.max(dp[i][j][1], up  + graph[i][j]);
+                } else {
+                    dp[i][j][1] = Math.max(dp[i][j][1], up  + graph[i][j]);
+                    dp[i][j][1] = Math.max(dp[i][j][1], dp[i][j+1][1]  + graph[i][j]);
+                }
+            }
+        }
+        System.out.println(Math.max(dp[N-1][M-1][0], dp[N-1][M-1][1]));
     }
 }
