a9d8ed26b2a1d37eda4a4a917e2b223dfae144b4
mikyeong Kwon <zxc_777@naver.com>
2026-04-24 08:59:21 +0900

    20260424

--- src/Main.java
+++ src/Main.java
@@ -1,102 +1,123 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx = new int[]{1,-1,0,0};
-    static int[] dy = new int[]{0,0,1,-1};
-    static int[] opposite = new int[]{1, 0, 3, 2};
-
-    static boolean[][][] visited;
     static int[][] graph;
-    static int[][] dp;
+    static int[][] types;
+    static int[] opps;
+    static int[][] results;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    static int N,M;
+    static boolean[][][] visited;
+    static int answer;
 
-    static int[][] infos;
-    static int answer = 0;
+    public static void main(String[] args) {
+        int[][] temp = new int[][]{{1, 0, 0, 0, 0, -1, -1}, {-1, 0, 0, 1, 0, 0, 1}};
+        Solution s = new Solution();
+        System.out.println(s.solution(temp));
+    }
 
     public int solution(int[][] grid) {
+        answer = 0;
         graph = grid;
-        visited = new boolean[graph.length][graph[0].length][2];
+        types = new int[][] {{}, {2, 3}, {0, 1}, {0,1,2,3}, {1,3}, {1,2},{0,2},{0,3}};
+        opps = new int[]{1, 0,3,2};
+        M = graph.length;
+        N = graph[0].length;
+        visited = new boolean[M][N][2];
+        results = new int[M][N];
+        visited[0][0][0] = true;
 
-        infos = new int[][]{{}, {2,3},{0,1},{0,1,2,3},{3,1},{1,2},{2,0},{3,0}};
-
-        dp = new int[graph.length][graph[0].length];
         for(int i = 0;i < graph.length; i++) {
             for(int j = 0;j < graph[0].length; j++) {
-                dp[i][j] = graph[i][j];
+                results[i][j] = graph[i][j];
             }
         }
-        dfs(0,0, 2);
+        dfs(0,0,2);
         return answer;
     }
 
-
-    private static void dfs(int x, int y, int out) {
-        if(x == graph.length - 1 && y == graph[0].length -1) {
-            if(crossCheck()) {
+    private static void dfs(int x, int y, int outDir) {
+        if(x == M-1 && y == N-1) {
+            if(check()) {
                 answer++;
             }
             return;
         }
-
-        int nx = x + dx[out];
-        int ny = y + dy[out];
-        int oppo = opposite[out];
+        int nx = x + dx[outDir];
+        int ny = y + dy[outDir];
+        int oppo = opps[outDir];
 
         if(isOut(nx, ny) || graph[nx][ny] == -1) return;
+        int newType = results[nx][ny];
+        int original = results[nx][ny];
 
-        int axis = (out == 0 || out == 1) ? 0 : 1; //0이면 가로, 1이면 세로
-        if(visited[nx][ny][axis]) return;
+        if(newType > 0) {
+            if(canConnect(oppo, newType)) {
+                for(int dir : types[newType]) {
+                    int axis = (dir == 2 || dir == 3) ? 0 : 1;
+                    if(oppo == dir || visited[nx][ny][axis]) continue;
 
-        int nextType = graph[nx][ny];
-        if(nextType > 0) {
-            if(canConnect(nx, ny, oppo)) {
-                visited[nx][ny][axis] = true;
-                for(int dir : infos[nextType]) {
-                    if(dir == oppo) continue;
-                    dfs(nx, ny, dir);
-                }
-                visited[nx][ny][axis] = false;
-            }
-        }
-        else if(nextType == 0) {
-            for(int t = 1; t <= 7; t++) {
-                if(canConnect(nx, ny, t)) {
+                    results[nx][ny] = newType;
                     visited[nx][ny][axis] = true;
-                    dp[nx][ny] = t;
-                    for(int dir : infos[nextType]) {
-                        if(oppo == dir) continue;
+
+                    if(newType != 3) {
                         dfs(nx, ny, dir);
                     }
-                    dp[nx][ny] = 0;
+                    else if(newType == 3 && dir == opps[oppo]) {
+                        dfs(nx, ny, dir);
+                    }
+
+                    results[nx][ny] = original;
                     visited[nx][ny][axis] = false;
                 }
             }
+        } else {
+            for(int i = 1; i <= 7; i++) {
+                if(canConnect(oppo, i)) {
+                    results[nx][ny] = i;
+
+                    for(int dir : types[i]) {
+                        int axis = (dir == 2 || dir == 3) ? 0 : 1;
+                        if(oppo == dir || visited[nx][ny][axis]) continue;
+
+                        visited[nx][ny][axis] = true;
+                        if(i != 3) {
+                            dfs(nx, ny, dir);
+                        }
+                        else if(i == 3 && dir == opps[oppo]) {
+                            dfs(nx, ny, dir);
+                        }
+                        visited[nx][ny][axis] = false;
+                    }
+                    results[nx][ny] = 0;
+                }
+            }
         }
-
     }
-    private static boolean crossCheck() {
-        for(int i = 0; i< dp.length; i++) {
-            for(int j =0; j < dp[i].length; j++) {
-                if(dp[i][j] != 3) continue;
+    private static boolean check() {
+        for(int i =0; i< results.length; i++) {
+            for(int j =0 ;j < results[0].length; j++) {
+                if(i == 0 && j == 0) continue;
+                if(i == M - 1 && j == N - 1) continue;
+                int type = results[i][j];
+                if(type <= 0) continue;
 
-                int nx;
-                int ny;
-                for(int k = 0; k< 4; k++) {
-                    nx = i + dx[k];
-                    ny = j + dy[k];
+                for(int t : types[type]) {
+                    int nx = i + dx[t];
+                    int ny = j + dy[t];
+                    int opp = opps[t];
 
-                    if(isOut(nx, ny)) return false;
-                    if(dp[nx][ny] <= 0 || !canConnect(nx, ny, opposite[k])) return false;
+                    if(isOut(nx, ny) || results[nx][ny] <= 0) return false;
+                    if(!canConnect(opp, results[nx][ny])) return false;
                 }
             }
         }
         return true;
     }
-    private static boolean canConnect(int nx, int ny, int dir) {
-        int num = graph[nx][ny];
-
-        int[] info = infos[num];
-        for(int in : info) {
-            if(in == dir) {
+    private static boolean canConnect(int dir, int type) {
+        for(int connect : types[type]) {
+            if(dir == connect) {
                 return true;
             }
         }
@@ -104,7 +125,7 @@ class Solution {
     }
 
     private static boolean isOut(int x, int y) {
-        if(x >= graph.length || x < 0 || y >= graph[0].length || y < 0) return true;
+        if(x >= M || x < 0 || y >= N || y < 0) return true;
         return false;
     }
 }
