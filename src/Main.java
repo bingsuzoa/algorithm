6578be697be8495b1eb8e7ea9da733c62642a7c2
mikyeong Kwon <zxc_777@naver.com>
2026-05-11 08:25:20 +0900

    20260511

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,76 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int m, int n, int h, int w, int[][] drops) {
-        int[][] times = new int[m][n];
+    static int[][] graph;
 
-        for(int i = 0; i < times.length; i++) {
-            Arrays.fill(times[i], Integer.MAX_VALUE);
+    public int[] solution(int m, int n, int h, int w, int[][] drops) {
+        graph = new int[m][n];
+
+        for(int i = 0; i < graph.length; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
         }
 
         for(int i = 0; i < drops.length; i++) {
             int r = drops[i][0];
             int c = drops[i][1];
-            times[r][c] = i + 1;
+            graph[r][c] = i+1;
         }
 
         int[][] rowMins = new int[m][n - w + 1];
-        int bestTime = -1;
-        int bestX = -1;
-        int bestY = -1;
 
-        //row sliding
-        for(int i = 0; i < m; i++) {
-            Deque<Integer> dq = new ArrayDeque<>();
-
+        for(int i = 0; i < m; i ++) {
+            Deque<Integer> dq = new LinkedList<>();
             for(int j = 0; j < n; j++) {
 
-                while(!dq.isEmpty() && times[i][j] <= times[i][dq.peekLast()]) {
+                while(!dq.isEmpty() && graph[i][dq.peekLast()] >= graph[i][j]) {
                     dq.pollLast();
                 }
                 dq.addLast(j);
 
-                if(j - dq.peekFirst() + 1 > w) {
+                if(!dq.isEmpty() && dq.peekFirst() < j - w + 1) {
                     dq.pollFirst();
                 }
 
                 if(j - w + 1 >= 0) {
-                    rowMins[i][j-w+1] = times[i][dq.peekFirst()];
+                    rowMins[i][j- w + 1] = graph[i][dq.peekFirst()];
                 }
             }
         }
 
-        //col sliding
+        int min = Integer.MIN_VALUE;
+        int ax = -1;
+        int ay = - 1;
         for(int j = 0; j < n - w + 1; j++) {
-            Deque<Integer> dq = new ArrayDeque<>();
-
-            for(int i = 0; i < m; i++) {
+            Deque<Integer> dq = new LinkedList<>();
+            for(int i = 0 ; i < m; i++) {
 
                 while(!dq.isEmpty() && rowMins[i][j] <= rowMins[dq.peekLast()][j]) {
                     dq.pollLast();
                 }
                 dq.addLast(i);
 
-                if(i - dq.peekFirst() + 1 > h) {
+                if(!dq.isEmpty() && i - h + 1 > dq.peekFirst()) {
                     dq.pollFirst();
                 }
 
                 if(i - h + 1 >= 0) {
-                    int currentTime = rowMins[dq.peekFirst()][j];
-
-                    int startX = i - h + 1;
-                    int startY = j;
-                    if(currentTime > bestTime) {
-                        bestTime = currentTime;
-                        bestX = startX;
-                        bestY = startY;
-                    }
-                    else if(currentTime == bestTime) {
-                        if(startX < bestX) {
-                            bestX = startX;
-                            bestY = startY;
+                    if(min < rowMins[dq.peekFirst()][j]) {
+                        min = rowMins[dq.peekFirst()][j];
+                        ax = i - h + 1;
+                        ay = j;
+                    } else if(min == rowMins[dq.peekFirst()][j]) {
+                        if(ax > i -h + 1) {
+                            ax = i - h + 1;
+                            ay = j;
                         }
-                        else if(startX == bestX && startY < bestY) {
-                            bestY = startY;
+                        else if(ax == i - h + 1 && ay > j) {
+                            ax = i - h + 1;
+                            ay = j;
                         }
                     }
                 }
             }
         }
-        return new int[]{bestX, bestY};
+        return new int[]{ax, ay};
     }
 }
