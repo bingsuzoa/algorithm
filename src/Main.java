9d6678e2261c40cba590533102b17c752472b283
mikyeong Kwon <zxc_777@naver.com>
2026-05-18 08:32:02 +0900

    20260518

--- src/Main.java
+++ src/Main.java
@@ -1,140 +1,142 @@
 import java.util.*;
 
 class Solution {
+    static int[] graph;
+    static long[] tmp;
+    static long[] tmptmp;
     public long[] solution(int[] arr, long l, long r) {
-        long K = getDurationSum(arr, l, r);
+        graph = arr;
 
-
-        //arr의 숫자가 바뀌는 순간의 인덱스를 저장한 배열만들기
-        long[] graph = getChangeArr(arr);
-
-        //변화가 있는 구간의 시작 인덱스를 저장한 리스트를 만들기
-        List<Long> list = getList(arr, graph, l, r);
-
-        //리스트 순회하면서 공차 확인
-        //sum getDurationSum(arr, start, end);
-        //start 1 end 3 : getNum(end) - getNum(start);
-        long answer = 0;
-        for(int i = 0; i < list.size() - 1; i++) {
-            long count = list.get(i +1) - list.get(i);
-
-            long start = list.get(i);
-            long end = start + (r - l);
-            long sum = getDurationSum(arr, start, end);
-
-            long dist = getNum(arr, end) - getNum(arr, start);
-
-            if(dist == 0 && K == sum) {
-                answer += count;
-            }
-            else if(dist != 0 && (K - sum) % dist == 0) {
-                long x = (K - sum) / dist;
-                if(x < count) {
-                    answer += 1;
-                }
-            }
-        }
-
-        return new long[]{K, answer};
-    }
-
-    private List<Long> getList(int[] arr, long[] graph, long l, long r) {
-        Set<Long> set = new HashSet<>();
-        List<Long> list = new ArrayList<>();
-
-        long dur = r - l;
-        long lastPos = getLast(arr);
-
-        if(1 + dur <= lastPos) {
-            list.add(1L);
-            set.add(1L);
-        }
-
-        if(lastPos - dur >= 0 && !set.contains(lastPos - dur + 1)) {
-            set.add(lastPos - dur + 1);
-            list.add(lastPos - dur + 1);
-        }
-
-        for(int i = 0; i < graph.length; i++) {
-            long pos = graph[i];
-
-            if(pos - dur >= 0 && !set.contains(pos - dur)) {
-                set.add(pos - dur);
-                list.add(pos - dur);
-            }
-            if(pos + dur <= lastPos && !set.contains(pos)) {
-                set.add(pos);
-                list.add(pos);
-            }
-        }
-        Collections.sort(list);
-        return list;
-    }
-
-
-    private long[] getChangeArr(int[] arr) {
-        long[] tmp = new long[arr.length - 1];
-        for(int i = 0; i < arr.length - 1; i++) {
+        //int[] temp;
+        tmp = new long[arr.length - 1];
+        for(int i =0 ; i < arr.length - 1; i++) {
             if(i - 1 >= 0) {
-                tmp[i] = arr[i] + tmp[i-1];
+                tmp[i] = tmp[i-1] + arr[i];
             } else {
                 tmp[i] = arr[i];
             }
         }
         for(int i =0 ; i < tmp.length; i++) {
-            tmp[i] += 1;
+            tmp[i]++;
         }
-        return tmp;
-    }
 
-
-    //어떤 인덱스에서의 숫자를 구한다.
-    private long getNum(int[] arr, long goal) {
-        long cur = 0;
-
-
-        for(int i = 0; i < arr.length; i++) {
-            if(cur + arr[i] < goal) {
-                cur += arr[i];
+        tmptmp = new long[arr.length];
+        for(int i =0 ; i  < tmptmp.length; i++) {
+            if(i - 1>= 0) {
+                tmptmp[i] = tmptmp[i-1] + ((long)arr[i] * arr[i]);
             } else {
-                return arr[i];
+                tmptmp[i] = (long)arr[i] * arr[i];
             }
         }
-        return arr[arr.length - 1];
+        long K = getDuration(l, r);
+        //list구하기
+        List<Long> list = new ArrayList<>();
+        Set<Long> set = new HashSet<>();
+        long dist = r - l;
+        long totalCount = getTotalCount();
+
+        //1 넣을 수 있는지 확인
+        if(1 + dist <= totalCount) {
+            list.add(1L);
+            set.add(1L);
+        }
+        //last 넣어주기
+        list.add(totalCount - dist + 1);
+        set.add(totalCount - dist + 1);
+
+        for(long value : tmp) {
+            if(value + dist <= totalCount && !set.contains(value)) {
+                set.add(value);
+                list.add(value);
+            }
+            if(value - dist >= 1 && !set.contains(value - dist)) {
+                set.add(value - dist);
+                list.add(value - dist);
+            }
+        }
+        Collections.sort(list);
+
+        long answer = 0;
+        //start
+        for(int i =0 ; i < list.size() - 1; i++) {
+            long value = list.get(i);
+            long startNum = getNum(value);
+            long endNum = getNum(value + dist);
+            long count = list.get(i+1) - list.get(i);
+
+            long tmpSum = getDuration(value, value+dist);
+
+            long diff = endNum - startNum;
+            if(diff == 0 && tmpSum == K) {
+                answer+= count;
+            }
+            else if(diff != 0) {
+                if((K - tmpSum) % diff == 0) {
+                    long tmpCount = (K - tmpSum) / diff;
+                    if(tmpCount >= 0 && tmpCount < count) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return new long[]{K, answer};
+
     }
-
-
-
-
-
-    private long getDurationSum(int[] arr, long l, long r) {
-        return getSum(arr, r) - getSum(arr, l - 1);
-    }
-
-    ///구간의 합을 구한다.
-    private long getSum(int[] arr, long pos) {
+    private long getTotalCount() {
         long sum = 0;
-        long cur = 0;
-        for(int i =0 ; i < arr.length; i++) {
-            int num = arr[i];
-
-            if(cur + num <= pos) {
-                sum += (long)num * num;
-                cur += num;
-            } else {
-                long remain = pos - cur;
-                sum += ((long)remain * num);
-                break;
-            }
+        for(int value : graph) {
+            sum += (value);
         }
         return sum;
     }
+    private long getDuration(long l, long r) {
+        return getSum(r) - getSum(l-1);
+    }
 
-    private long getLast(int[] arr) {
-        long sum = 0;
-        for(int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+    //어느 위치에서의 합
+    private long getSum(long pos) {
+        int start = 0;
+        int end = tmp.length - 1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if(tmp[mid] <= pos) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
+        int idx = end + 1;
+
+        long count = 0;
+        long sum = 0;
+
+        if(idx - 1 >= 0) {
+            count += (tmp[idx - 1] - 1);
+            sum += tmptmp[idx - 1];
+        }
+        long remain = pos - count;
+        sum += (long)remain * getNum(pos);
         return sum;
+    }
+
+    //어느 위치에서의 숫자
+    private int getNum(long pos) {
+
+        int start = 0;
+        int end = tmp.length - 1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if(tmp[mid] <= pos) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return graph[end + 1];
     }
 }
