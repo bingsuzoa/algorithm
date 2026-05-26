46586ee348398a54f222b15f10ec8b616f16794a
bingsuzoa <zxc_777@naver.com>
2026-05-26 09:02:03 +0900

    [Passed] 사다리 타기 (200ms, 16MB)

--- trail4/사다리 타기/ladder-game.java
+++ trail4/사다리 타기/ladder-game.java
@@ -1,106 +1,76 @@
 import java.util.Scanner;
 import java.util.*;
 
-
 public class Main {
-    static int N,M;
     static List<int[]> initList;
-    static int[] initResult;
+    static int N,M;
+    static int[] initAnswer;
     static int answer;
-    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         N = n; M = m;
 
-        //List<int[]> 를 구한다.
         initList = new ArrayList<>();
-        for(int i = 0;i < M; i++) {
+        for(int i = 0; i < m; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
-            initList.add(new int[]{a, b});
+            initList.add(new int[]{a,b});
         }
-        
-        //초기화 정답 구하기
         boolean[] initChecked = new boolean[m];
         Arrays.fill(initChecked, true);
-        initResult = getResult(makeList(initChecked));
+        initAnswer = getResult(initChecked);
         answer = M;
-    
-        //dfs(list.size()만큼 true, false 전수조사)
-        dfs(0, new boolean[M]);
+        dfs(0,0, new boolean[M]);
         System.out.println(answer);
     }
-    private static void dfs(int cur, boolean[] tmp) {
-        if(cur == tmp.length) {
-            int[] tmpAnswer = getResult(makeList(tmp));
-            if(isTrue(tmpAnswer)) {
-                answer = Math.min(answer, getCount(tmp));
+    private static void dfs(int cur, int count, boolean[] checked) {
+        if(isTrue(getResult(checked))) {
+                answer = Math.min(answer, count);
             }
-            return;
-        }
-
-        for(int i = 0; i <= 1; i++) {
-            tmp[cur] = i == 0 ? true : false;
-            dfs(cur + 1, tmp);
+            
+        for(int i = cur; i < M; i++) {
+            checked[i] = true;
+            dfs(i + 1, count + 1, checked);
+            checked[i] = false;
         }
     }
-    private static boolean isTrue(int[] tmp) {
-        for(int i = 0;i < tmp.length; i++) {
-            if(tmp[i] != initResult[i]) return false;
+    private static boolean isTrue(int[] result) {
+        for(int i =0 ;i < result.length; i++) {
+            if(result[i] != initAnswer[i]) {
+                return false;
+            }
         }
         return true;
     }
-    private static int getCount(boolean[] tmp) {
-        int count = 0;
-        for(int i = 0;i < tmp.length; i++) {
-            if(tmp[i]) count++;
-        }
-        return count;
-    }
-    private static List<int[]>[] makeList(boolean[] checked) {
-        List<int[]>[] list = new ArrayList[N+1];
-        for(int i =0;i < list.length; i++) {
-            list[i] = new ArrayList<>();
-        }
-        for(int i = 0; i < M; i++){
-            if(!checked[i]) continue;
-            int[] tmp = initList.get(i);
-            int a = tmp[0];
-            int b = tmp[1];
-            list[a].add(new int[]{b, a+1});
-            if(a+1 < list.length) {
-                list[a+1].add(new int[]{b, a});
-            }
-        }
-        for(int i = 0;i < list.length; i++) {
-            Collections.sort(list[i], (o1,o2) -> {
-                return o1[0] - o2[0];
-            });
-        };
-        return list;
-    }
-     //true에 해당하는 List<int[]>[] 구하기
-    private static int[] getResult(List<int[]>[] list) {
-        int[] result = new int[list.length];
-
-        for(int person = 1; person <= N; person++) {
-            int ch = 1;
-            int pos = person;
-            while(ch <= 15) {
-                for(int[] next : list[pos]) {
-                    int nextCh = next[0];
-                    int to = next[1];
-                    if(nextCh == ch) {
-                        pos = to;
-                        break;
-                    }
+    private static int[] makeResult(boolean[][] graph) {
+        int[] result = new int[N+1];
+        for(int per = 1; per <= N; per++) {
+            int pos = per;
+            for(int ch = 1; ch < 16; ch++) {
+                if(graph[ch][pos]) {
+                    pos++;
                 }
-                ch++;
+                else if(pos > 1 && graph[ch][pos - 1]) {
+                    pos --;
+                }
             }
-            result[pos] = person;
+            result[pos] = per;
         }
-        return result;    
+        return result;
+    }
+    
+    private static int[] getResult(boolean[] checked) {
+        boolean[][] graph = new boolean[16][N+1];
+        for(int i = 0;i < checked.length; i++) {
+            if(checked[i]) {
+                int[] tmp = initList.get(i);
+                int a = tmp[0];
+                int b = tmp[1];
+                graph[b][a] = true;
+            }
+        }
+        return makeResult(graph);
     }
 }
