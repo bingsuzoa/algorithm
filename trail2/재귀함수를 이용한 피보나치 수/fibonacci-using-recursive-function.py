dffc322bc4f88c8c69a856a9ebe485bbf5e68534
bingsuzoa <zxc_777@naver.com>
2026-06-05 08:14:12 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (50ms, 16MB)

--- trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
+++ trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
@@ -2,22 +2,20 @@ N = int(input())
 
 # Please write your code here.
 arr = [0] * (N + 1)
-if N > 2 :
-    arr[1] = 1
-    arr[2] = 1
 
 def dfs(cur, N) :
+    if N == 1 :
+        return 1
+    if N == 2 :
+        return 1
     if cur > N :
-        return
+        return arr[N]
+    
+    if cur <= 2 :
+        arr[cur] = 1
+        return dfs(cur + 1, N)
+    else :
+        arr[cur] = arr[cur - 1] + arr[cur - 2]
+        return dfs(cur + 1, N)
 
-    arr[cur] = arr[cur - 1] + arr[cur - 2]
-    dfs(cur + 1, N)
-
-
-if N == 1 :
-    print("1")
-elif N == 2 :
-    print("1")
-else :
-    dfs(3, N)
-    print(arr[N])
+print(dfs(1, N))
