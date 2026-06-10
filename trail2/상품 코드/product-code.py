e38a4241a1aa223c13e0ccdaeb7616379b6c3c2f
bingsuzoa <zxc_777@naver.com>
2026-06-11 07:24:54 +0900

    [Passed] 상품 코드 (49ms, 16MB)

---
+++ trail2/상품 코드/product-code.py
@@ -0,0 +1,15 @@
+product_name, product_code = input().split()
+product_code = int(product_code)
+
+# Please write your code here.
+class product :
+    def __init__(self, name, code) :
+        self.name = name
+        self.code = code
+
+
+product1 = product("codetree", 50)
+product2 = product(product_name, product_code)
+
+print(f"product {product1.code} is {product1.name}")
+print(f"product {product2.code} is {product2.name}")
