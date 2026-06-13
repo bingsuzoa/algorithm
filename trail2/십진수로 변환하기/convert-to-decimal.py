c004394e1b810243cbffba2cc71960d7271f1c02
bingsuzoa <zxc_777@naver.com>
2026-06-13 09:43:07 +0900

    [Passed] 십진수로 변환하기 (47ms, 16MB)

--- trail2/십진수로 변환하기/convert-to-decimal.py
+++ trail2/십진수로 변환하기/convert-to-decimal.py
@@ -1,12 +1,8 @@
 binary = input()
 
 
-binary = binary[::-1]
-
 num = 0
-result = 0
 for e in binary :
-    result += int(e) * pow(2, num)
-    num += 1
+    num = num * 2 + int(e)  
 
-print(result)
+print(num)
