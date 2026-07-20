9725e3f28d7f5e1b1fffdfa48afdbf639156e8fd
bingsuzoa <zxc_777@naver.com>
2026-07-20 20:48:28 +0900

    [Passed] 작은 구슬의 이동 (48ms, 16MB)

--- trail2/작은 구슬의 이동/small-marble-movement.py
+++ trail2/작은 구슬의 이동/small-marble-movement.py
@@ -1,38 +1,25 @@
 n, t = map(int, input().split())
+sx, sy, dir = input().split()
+sx = int(sx)
+sy = int(sy)
 
-x, y, dir = input().split()
-x = int(x)
-y = int(y)
+graph = [[0] * (n+1) for _ in range(n+1)]
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
+dirMap = {"D" : 0, "U" : 1, "R" : 2, "L" : 3}
+changeMap = {0 : 1, 1 : 0, 2 : 3, 3 : 2}
 
+idx = dirMap[dir]
 for _ in range(t) :
-    if dir == 'L' :
-        nx = x + dx[3]
-        ny = y + dy[3]
-    elif dir == 'R' :
-        nx = x + dx[2]
-        ny = y + dy[2]
-    elif dir == 'U' :
-        nx = x + dx[1]
-        ny = y + dy[1]
-    elif dir == 'D' :
-        nx = x + dx[0]
-        ny = y + dy[0]
-   
-    if nx <= 0 or nx > n or ny <= 0 or ny > n :
-        if dir == 'L' :
-            dir = 'R'
-        elif dir == 'R' :
-            dir = 'L'
-        elif dir == 'U' :
-            dir = 'D'
-        elif dir == 'D' :
-            dir = 'U'
-    else :
-        x = nx
-        y = ny        
+    nx = sx + dx[idx]
+    ny = sy + dy[idx]
 
-print(x, y)
+    if nx <= 0 or nx >= n+1 or ny <= 0 or ny >= n+1 :
+        idx = changeMap[idx]
+    else :
+        sx = nx
+        sy = ny
+
+print(sx, sy)
