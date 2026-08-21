c0d4cf6d359fd250dcc8945b039afbb251c17736
bingsuzoa <zxc_777@naver.com>
2026-08-22 08:53:06 +0900

    [Passed] 아름다운 수열 2 (66ms, 18MB)

--- trail2/아름다운 수열 2/beautiful-sequence-2.py
+++ trail2/아름다운 수열 2/beautiful-sequence-2.py
@@ -16,12 +16,30 @@ def check(arr1, arr2) :
     return True
 
 ans = 0
-for i in range(len(A) - M + 1) :
-    tmpArr = [0] * 101
-    for j in range(i, i + M) :
-        tmpArr[A[j]] += 1
-    if check(ansArr, tmpArr) :
-        ans += 1
+# for i in range(len(A) - M + 1) :
+#     tmpArr = [0] * 101
+#     for j in range(i, i + M) :
+#         tmpArr[A[j]] += 1
+#     if check(ansArr, tmpArr) :
+#         ans += 1
+
+left = 0
+right = 0
+cnt = 0
+tmpArr = [0] * 101
+while left <= len(A) - M :
+    if right < len(A) and cnt < M :
+        tmpArr[A[right]] += 1
+        cnt += 1
+        right += 1
+        if cnt == M :
+            # print(f"{left}, {right}")
+            if check(ansArr, tmpArr) :
+                ans += 1
+            tmpArr[A[left]] -= 1
+            cnt -= 1
+            left += 1
+
 
 
 print(ans)
